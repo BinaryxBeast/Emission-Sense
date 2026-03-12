85ee7e657a188f4789126d81fb0222790d4ef0e7
BinaryxBeast <prog.rushii@gmail.com>
2026-03-12 18:48:14 +0530

    chore: remove .next build artifacts from tracking

--- Desktop/asep_project/.next/dev/static/chunks/[project]_app_favicon_ico_mjs_2d30c0ac._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/a7034_next_dist_04d830ff._.js"
-],
-    source: "dynamic"
-});
--- Desktop/asep_project/.next/dev/static/chunks/[project]_app_page_tsx_da771afc._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__000b1df6._.js",
-  "static/chunks/a7034_next_dist_compiled_react_ee810e84._.js"
-],
-    source: "dynamic"
-});
